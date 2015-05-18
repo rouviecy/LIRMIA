899b0a0a318a28def72220b0bf5ed4485da8bec8
rouviecy <pi@cyril-rouviere.fr>
2015-05-18 12:35:47 -0500

    Implemented interface with acoustic modem

--- src/parts/Acoustic_modem.h
+++ src/parts/Acoustic_modem.h
@@ -14,11 +14,18 @@
 #include "../core/ComThread.h"
 #include "../interfaces/FSM.h"
 #include "../interfaces/FSMDraw.h"
+#include <queue>
+
+#define MAX_COMMANDS_IN_BUFFER 10
 
 enum state_com_t {
 	IDLE,
 	MASTER_BAD_COM, MASTER_GOOD_COM, MASTER_WANT_X, MASTER_WANT_Y, MASTER_WANT_Z, MASTER_WANT_YPR, MASTER_WANT_COMMAND, MASTER_CONFIRM_XYZ, MASTER_CONFIRM_YPR, MASTER_CONFIRM_COMMAND,
 	SLAVE_GOOD_COM, SLAVE_IMPROVE_ANGLE, SLAVE_IMPROVE_DISTANCE, SLAVE_SEND_X, SLAVE_SEND_Y, SLAVE_SEND_Z, SLAVE_SEND_YPR, SLAVE_EXEC_COMMAND
+};
+
+enum command_t {
+	COMMAND_REMOTE_ONOFF, COMMAND_STAY, COMMAND_EXPLORE, COMMAND_NOFOLLOW, COMMAND_UP, COMMAND_DOWN
 };
 
 class Acoustic_modem : public ComThread{
@@ -28,28 +35,47 @@ public:
 	Acoustic_modem();
 	~Acoustic_modem();
 
+	void Get_xyz();
+	void Get_ypr();
+	void Send_command(command_t command);
+
 private:
 
 	FSM fsm;
 	FSMDraw drawer;
 
+	// ComThread
 	void On_start();
 	void Job();
 	void IO();
-	
+
+	// Inputs
+	float t;
 	float xyz[3];
 	float thxyz[3];
 
+	// Outputs
 	int fsm_state;
+	float modem_txyz[4];
+	float modem_typr[4];
 
+	// Requests
+	std::mutex mu;
+	bool need_xyz;
+	bool need_ypr;
+	std::queue <command_t> command_buffer;
+
+	// Common actions
 	static void Back_to_idle			(void* obj);
 
+	// Master actions
 	static void Send_key_and_stabilize		(void* obj);
 	static void Send_request_xyz			(void* obj);
 	static void Send_request_ypr			(void* obj);
 	static void Send_request_command		(void* obj);
 	static void Send_same_msg			(void* obj);
 
+	// Slave actions
 	static void Stabilize_without_obliquity		(void* obj);
 	static void Stabilize_with_little_obliquity	(void* obj);
 	static void Change_precession			(void* obj);
