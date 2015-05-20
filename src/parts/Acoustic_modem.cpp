2b76dc931e7a4f4f141395b3ec1c633d015b22c2
rouviecy <pi@cyril-rouviere.fr>
2015-05-20 10:30:39 -0500

    Implemented Master side of acoustic communication FSM

--- src/parts/Acoustic_modem.cpp
+++ src/parts/Acoustic_modem.cpp
@@ -5,8 +5,19 @@ using namespace std;
 Acoustic_modem::Acoustic_modem() : ComThread(){
 
 	fsm_state = IDLE;
+
+	last_t_msg = -1.;
 	need_xyz = false;
 	need_ypr = false;
+
+	modem_txyz[0] = -1.; modem_txyz[1] = 0.; modem_txyz[2] = 0.; modem_txyz[3] = 0.;
+	modem_typr[0] = -1.; modem_typr[1] = 0.; modem_typr[2] = 0.; modem_typr[3] = 0.;
+
+	modem_stab = false;
+	for(int i = 0; i < 3; i++){
+		order_xyz_com[i] = 0.;
+		order_thxyz_com[i] = 0.;
+	}
 
 	#if defined(ENABLE_MODEM) and not defined(ENABLE_SERIAL_RS232)
 		cout << "[Warning] You are trying to use acoustic modem without serial : modem will be disabled" << endl;
@@ -22,7 +33,6 @@ Acoustic_modem::Acoustic_modem() : ComThread(){
 		fsm.Add_state("master_want_y",		MASTER_WANT_Y);
 		fsm.Add_state("master_want_z",		MASTER_WANT_Z);
 		fsm.Add_state("master_want_ypr",	MASTER_WANT_YPR);
-		fsm.Add_state("master_want_command",	MASTER_WANT_COMMAND);
 		fsm.Add_state("master_confirm_xyz",	MASTER_CONFIRM_XYZ);
 		fsm.Add_state("master_confirm_ypr",	MASTER_CONFIRM_YPR);
 		fsm.Add_state("master_confirm_command",	MASTER_CONFIRM_COMMAND);
@@ -91,6 +101,7 @@ Acoustic_modem::Acoustic_modem() : ComThread(){
 		fsm.Add_transition(	"master_want_x",		"master_want_y",		"receive_good_data",		"",		"send_same_msg",			(void*) this);
 		fsm.Add_transition(	"master_want_y",		"master_want_z",		"receive_good_data",		"",		"send_same_msg",			(void*) this);
 		fsm.Add_transition(	"master_want_z",		"master_confirm_xyz",		"receive_good_data",		"",		"send_same_msg",			(void*) this);
+		fsm.Add_transition(	"master_want_x",		"master_bad_com",		"receive_wrong_msg",		"",		"send_key_and_stabilize",		(void*) this);
 		fsm.Add_transition(	"master_want_y",		"master_bad_com",		"receive_wrong_msg",		"",		"send_key_and_stabilize",		(void*) this);
 		fsm.Add_transition(	"master_want_z",		"master_bad_com",		"receive_wrong_msg",		"",		"send_key_and_stabilize",		(void*) this);
 		fsm.Add_transition(	"master_confirm_xyz",		"master_bad_com",		"receive_wrong_msg",		"",		"send_key_and_stabilize",		(void*) this);
@@ -99,12 +110,12 @@ Acoustic_modem::Acoustic_modem() : ComThread(){
 		// Master wants YPR
 		fsm.Add_transition(	"master_good_com",		"master_want_ypr",		"want_ypr",			"",		"send_request_ypr",			(void*) this);
 		fsm.Add_transition(	"master_want_ypr",		"master_confirm_ypr",		"receive_good_data",		"",		"send_same_msg",			(void*) this);
+		fsm.Add_transition(	"master_want_ypr",		"master_bad_com",		"receive_wrong_msg",		"",		"send_key_and_stabilize",		(void*) this);
 		fsm.Add_transition(	"master_confirm_ypr",		"master_bad_com",		"receive_wrong_msg",		"",		"send_key_and_stabilize",		(void*) this);
 		fsm.Add_transition(	"master_confirm_ypr",		"idle",				"receive_confirm_msg",		"",		"back_to_idle",				(void*) this);
 
 		// Master wants to command
-		fsm.Add_transition(	"master_good_com",		"master_want_command",		"want_command",			"",		"send_request_command",			(void*) this);
-		fsm.Add_transition(	"master_want_command",		"master_confirm_command",	"receive_good_data",		"",		"send_same_msg",			(void*) this);
+		fsm.Add_transition(	"master_good_com",		"master_confirm_command",	"want_command",			"",		"send_request_command",			(void*) this);
 		fsm.Add_transition(	"master_confirm_command",	"master_bad_com",		"receive_wrong_msg",		"",		"send_key_and_stabilize",		(void*) this);
 		fsm.Add_transition(	"master_confirm_command",	"idle",				"receive_confirm_msg",		"",		"back_to_idle",				(void*) this);
 
@@ -147,7 +158,10 @@ Acoustic_modem::Acoustic_modem() : ComThread(){
 
 Acoustic_modem::~Acoustic_modem(){}
 
-void Acoustic_modem::On_start(){}
+void Acoustic_modem::On_start(){
+// TODO : lancement d'un thread de réception de messages	
+	
+}
 
 void Acoustic_modem::IO(){
 	Link_input("t",			COMFLOAT,	1, &t);
@@ -157,10 +171,127 @@ void Acoustic_modem::IO(){
 	Link_output("fsm_modem",	COMINT,		1, &fsm_state);
 	Link_output("modem_txyz",	COMFLOAT,	4, modem_txyz);
 	Link_output("modem_typr",	COMFLOAT,	4, modem_txyz);
+	Link_output("modem_stab",	COMBOOL,	1, &modem_stab);
+	Link_output("order_xyz_com",	COMFLOAT,	3, order_xyz_com);
+	Link_output("order_thxyz_com",	COMFLOAT,	3, order_thxyz_com);
 }
 
 void Acoustic_modem::Job(){
 	Critical_receive();
+	#if defined(ENABLE_MODEM) and defined(ENABLE_SERIAL_RS232)
+		switch(fsm_state){
+			case IDLE:
+				if(command_buffer.size() > 0 || need_xyz || need_ypr){
+					for(int i = 0; i < 3; i++){
+						order_xyz_com[i] = xyz[i];
+						order_thxyz_com[i] = thxyz[i];
+					}
+					fsm.Call_event("want_something");
+				}
+				break;
+			case MASTER_BAD_COM:
+				while(in_msg_queue.size() > 0){
+					if(Compare_msg(in_msg_queue.front(), ACOUSTIC_MSG_KEY)){
+						fsm.Call_event("receive_good_key_confirm");
+						break;
+					}
+					in_msg_queue.pop();
+				}
+				if(t - last_t_msg < ACOUSTIC_DT_REPEAT_KEY){
+					last_t_msg = t;
+					fsm.Call_event("want_something");
+				}
+				break;
+			case MASTER_GOOD_COM:
+				if	(command_buffer.size() > 0)	{fsm.Call_event("want_command");								break;}
+				if	(need_xyz)			{fsm.Call_event("want_xyz");		mu.lock(); need_xyz = false;		mu.unlock();	break;}
+				if	(need_ypr)			{fsm.Call_event("want_ypr");		mu.lock(); need_ypr = false;		mu.unlock();	break;}
+				break;
+			case MASTER_WANT_X:	Process_data(ACOUSTIC_HEADER_X, &(tmp_xyz[1]));	break;
+			case MASTER_WANT_Y:	Process_data(ACOUSTIC_HEADER_Y, &(tmp_xyz[2]));	break;
+			case MASTER_WANT_Z:	Process_data(ACOUSTIC_HEADER_Z, &(tmp_xyz[3]));	break;
+			case MASTER_CONFIRM_XYZ:
+				if(in_msg_queue.size() > 0){
+					last_msg = in_msg_queue.front();
+					in_msg_queue.pop();
+					if(Compare_msg(last_msg, ACOUSTIC_CONFIRM_XYZ)){
+						modem_txyz[0] = t;
+						modem_txyz[1] = tmp_xyz[0];
+						modem_txyz[2] = tmp_xyz[1];
+						modem_txyz[3] = tmp_xyz[2];
+						Critical_send();
+						fsm.Call_event("receive_confirm_msg");
+					}
+					else{
+						fsm.Call_event("receive_wrong_confirm");
+					}
+				}
+				break;
+			case MASTER_WANT_YPR:
+				if(in_msg_queue.size() > 0){
+					last_msg = in_msg_queue.front();
+					in_msg_queue.pop();
+					if(last_msg[0] == ACOUSTIC_HEADER_YPR){
+						tmp_ypr[0] = Decode_acoustic_msg(in_msg_queue.front(), 1, 1) * 0.706 - 90.;
+						tmp_ypr[1] = Decode_acoustic_msg(in_msg_queue.front(), 2, 1) * 1.412 - 180.;
+						tmp_ypr[2] = Decode_acoustic_msg(in_msg_queue.front(), 3, 1) * 1.412 - 180.;
+						fsm.Call_event("receive_good_data");
+					}
+					else{
+						fsm.Call_event("receive_wrong_confirm");
+					}
+				}
+				break;
+			case MASTER_CONFIRM_YPR:
+				if(in_msg_queue.size() > 0){
+					last_msg = in_msg_queue.front();
+					in_msg_queue.pop();
+					if(Compare_msg(last_msg, ACOUSTIC_CONFIRM_YPR)){
+						modem_typr[0] = t;
+						modem_typr[1] = tmp_ypr[0];
+						modem_typr[2] = tmp_ypr[1];
+						modem_typr[3] = tmp_ypr[2];
+						Critical_send();
+						fsm.Call_event("receive_confirm_msg");
+					}
+					else{
+						fsm.Call_event("receive_wrong_confirm");
+					}
+				}
+				break;
+			case MASTER_CONFIRM_COMMAND:
+				if(in_msg_queue.size() > 0){
+					last_msg = in_msg_queue.front();
+					in_msg_queue.pop();
+					string expected_answer;
+					switch(command_buffer.front()){
+						case COMMAND_REMOTE_ONOFF:	expected_answer = ACOUSTIC_MSG_COMMAND_REMOTE_ONOFF;	break;
+						case COMMAND_STAY:		expected_answer = ACOUSTIC_MSG_COMMAND_STAY;		break;
+						case COMMAND_EXPLORE:		expected_answer = ACOUSTIC_MSG_COMMAND_EXPLORE;		break;
+						case COMMAND_NOFOLLOW:		expected_answer = ACOUSTIC_MSG_COMMAND_NOFOLLOW;	break;
+						case COMMAND_UP:		expected_answer = ACOUSTIC_MSG_COMMAND_UP;		break;
+						case COMMAND_DOWN:		expected_answer = ACOUSTIC_MSG_COMMAND_DOWN;		break;
+						default:			expected_answer = "";					break;
+					}
+					if(Compare_msg(last_msg, expected_answer)){
+						modem_typr[0] = t;
+						modem_typr[1] = tmp_ypr[0];
+						modem_typr[2] = tmp_ypr[1];
+						modem_typr[3] = tmp_ypr[2];
+						Critical_send();
+						command_buffer.pop();
+						fsm.Call_event("receive_confirm_msg");
+					}
+					else{
+						fsm.Call_event("receive_wrong_confirm");
+					}
+				}
+				break;
+			default:
+				cout << "[Warning] Undefined state for acoustic communication FSM" << endl;
+				break;
+		}
+	#endif
 }
 
 void Acoustic_modem::Get_xyz(){mu.lock(); need_xyz = true; mu.unlock();}
@@ -173,12 +304,77 @@ void Acoustic_modem::Send_command(command_t command){
 	}
 }
 
-void Acoustic_modem::Back_to_idle(void* obj){}
+bool Acoustic_modem::Compare_msg(string msg1, string msg2){
+	if(msg1.length() < 4 || msg2.length() < 4){return false;}
+	return msg1.compare(msg2) == 0;
+}
 
-void Acoustic_modem::Send_key_and_stabilize(void* obj){}
-void Acoustic_modem::Send_request_xyz(void* obj){}
-void Acoustic_modem::Send_request_ypr(void* obj){}
-void Acoustic_modem::Send_request_command(void* obj){}
+float Acoustic_modem::Decode_acoustic_msg(std::string msg, int begin, int end){
+	float result = 0.;
+	try{result = stof(msg.substr(begin, end));
+	}catch(std::exception const & e){}
+	return result;
+}
+
+void Acoustic_modem::Process_data(char wanted_header, float* destination){
+	if(in_msg_queue.size() > 0){
+		last_msg = in_msg_queue.front();
+		in_msg_queue.pop();
+		if(in_msg_queue.front()[0] == wanted_header){
+			*destination = Decode_acoustic_msg(last_msg, 1, 3);
+			fsm.Call_event("receive_good_data");
+		}
+		else{
+			fsm.Call_event("receive_wrong_confirm");
+		}
+	}
+}
+
+void Acoustic_modem::Get_acoustic_msg(){
+	// TODO ----------appel par thread séparé------------------
+}
+
+void Acoustic_modem::Send_acoustic_msg(std::string msg){
+	// TODO
+}
+
+// TODO ...
+
+void Acoustic_modem::Back_to_idle(void* obj){
+	Acoustic_modem* self = (Acoustic_modem*) obj;
+	self->modem_stab = false;
+}
+
+void Acoustic_modem::Send_key_and_stabilize(void* obj){
+	Acoustic_modem* self = (Acoustic_modem*) obj;
+	self->modem_stab = true;
+	self->Critical_send();
+	self->Send_acoustic_msg(ACOUSTIC_MSG_KEY);
+}
+
+void Acoustic_modem::Send_request_xyz(void* obj){
+	Acoustic_modem* self = (Acoustic_modem*) obj;
+	self->Send_acoustic_msg(ACOUSTIC_MSG_REQUEST_XYZ);
+}
+
+void Acoustic_modem::Send_request_ypr(void* obj){
+	Acoustic_modem* self = (Acoustic_modem*) obj;
+	self->Send_acoustic_msg(ACOUSTIC_MSG_REQUEST_YPR);	
+}
+
+void Acoustic_modem::Send_request_command(void* obj){
+	Acoustic_modem* self = (Acoustic_modem*) obj;
+	switch(self->command_buffer.front()){
+		case COMMAND_REMOTE_ONOFF:	self->Send_acoustic_msg(ACOUSTIC_MSG_COMMAND_REMOTE_ONOFF);	break;
+		case COMMAND_STAY:		self->Send_acoustic_msg(ACOUSTIC_MSG_COMMAND_STAY);		break;
+		case COMMAND_EXPLORE:		self->Send_acoustic_msg(ACOUSTIC_MSG_COMMAND_EXPLORE);		break;
+		case COMMAND_NOFOLLOW:		self->Send_acoustic_msg(ACOUSTIC_MSG_COMMAND_NOFOLLOW);		break;
+		case COMMAND_UP:		self->Send_acoustic_msg(ACOUSTIC_MSG_COMMAND_UP);		break;
+		case COMMAND_DOWN:		self->Send_acoustic_msg(ACOUSTIC_MSG_COMMAND_DOWN);		break;
+		default:											break;
+	}
+}
+
 void Acoustic_modem::Send_same_msg(void* obj){}
 
 void Acoustic_modem::Stabilize_without_obliquity(void* obj){}
--- src/parts/Acoustic_modem.h
+++ src/parts/Acoustic_modem.h
@@ -16,11 +16,29 @@
 #include "../interfaces/FSMDraw.h"
 #include <queue>
 
-#define MAX_COMMANDS_IN_BUFFER 10
+#define MAX_COMMANDS_IN_BUFFER			10
+#define ACOUSTIC_DT_REPEAT_KEY			5.
+#define ACOUSTIC_MSG_KEY			string("4242")
+#define ACOUSTIC_MSG_REQUEST_XYZ		string("rxyz")
+#define ACOUSTIC_MSG_REQUEST_YPR		string("rypr")
+#define ACOUSTIC_MSG_COMMAND_REMOTE_ONOFF	string("rrem")
+#define ACOUSTIC_MSG_COMMAND_STAY		string("rsta")
+#define ACOUSTIC_MSG_COMMAND_EXPLORE		string("rexp")
+#define ACOUSTIC_MSG_COMMAND_NOFOLLOW		string("rnof")
+#define ACOUSTIC_MSG_COMMAND_UP			string("rup0")
+#define ACOUSTIC_MSG_COMMAND_DOWN		string("rdow")
+#define ACOUSTIC_HEADER_X			'x'
+#define ACOUSTIC_HEADER_Y			'y'
+#define ACOUSTIC_HEADER_Z			'z'
+#define ACOUSTIC_HEADER_YPR			'a'
+#define ACOUSTIC_HEADER_COMMAND			'c'
+#define ACOUSTIC_CONFIRM_XYZ			string("txyz")
+#define ACOUSTIC_CONFIRM_YPR			string("typr")
+#define ACOUSTIC_CONFIRM_COMMAND		string("tcom")
 
 enum state_com_t {
 	IDLE,
-	MASTER_BAD_COM, MASTER_GOOD_COM, MASTER_WANT_X, MASTER_WANT_Y, MASTER_WANT_Z, MASTER_WANT_YPR, MASTER_WANT_COMMAND, MASTER_CONFIRM_XYZ, MASTER_CONFIRM_YPR, MASTER_CONFIRM_COMMAND,
+	MASTER_BAD_COM, MASTER_GOOD_COM, MASTER_WANT_X, MASTER_WANT_Y, MASTER_WANT_Z, MASTER_WANT_YPR, MASTER_CONFIRM_XYZ, MASTER_CONFIRM_YPR, MASTER_CONFIRM_COMMAND,
 	SLAVE_GOOD_COM, SLAVE_IMPROVE_ANGLE, SLAVE_IMPROVE_DISTANCE, SLAVE_SEND_X, SLAVE_SEND_Y, SLAVE_SEND_Z, SLAVE_SEND_YPR, SLAVE_EXEC_COMMAND
 };
 
@@ -58,12 +76,27 @@ private:
 	int fsm_state;
 	float modem_txyz[4];
 	float modem_typr[4];
+	bool modem_stab;
+	float order_xyz_com[3];
+	float order_thxyz_com[3];
 
 	// Requests
 	std::mutex mu;
+	float last_t_msg;
+	float tmp_xyz[3];
+	float tmp_ypr[3];
 	bool need_xyz;
 	bool need_ypr;
 	std::queue <command_t> command_buffer;
+
+	// Acoustic communication
+	std::queue <std::string> in_msg_queue;
+	std::string last_msg;
+	bool Compare_msg(std::string msg1, std::string msg2);
+	float Decode_acoustic_msg(std::string msg, int begin, int end);
+	void Process_data(char wanted_header, float* destination);
+	void Get_acoustic_msg();
+	void Send_acoustic_msg(std::string msg);
 
 	// Common actions
 	static void Back_to_idle			(void* obj);
