cd3c43113c2b5861de2ee21e6f901d5c673babe6
rouviecy <pi@cyril-rouviere.fr>
2015-06-12 16:32:36 -0500

    Servo motor command in Arduino

--- src/arduino_unknown.ino
+++ src/arduino_unknown.ino
@@ -1,13 +1,17 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
 SoftwareSerial GPS_Serial(10, 11); // RX, TX
 
 int order;
+Servo rudder;
 
 void setup(){
     Serial.begin(4800);
     GPS_Serial.begin(4800);
     order = 0;
+    rudder.attach(9); // Direction servo motor is on digital pin 9
+    rudder.write(90);
 }
 
 void loop(){
@@ -15,9 +19,9 @@ void loop(){
         Serial.write(GPS_serial.read());
     }
     while(Serial.available(){
-        order = (int) Serial.read();
+        order = (int) Serial.read() - 127; // incoming order from -127 to +127
+        rudder.write(90 + (order * 90) / 127);
     }
-// order stuff
     delay(200);
 }
 
