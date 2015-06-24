6cf6b2dd24181d4f94f02d021d57ba0e335e26f9
rouviecy <pi@cyril-rouviere.fr>
2015-06-24 16:52:35 -0500

    Can modify HSV filter for pipeline detection unit testing

--- src/units_testing/pipeline.cpp
+++ src/units_testing/pipeline.cpp
@@ -10,14 +10,21 @@ int main(int argc, char* argv[]){
 	hsv.Create_HSV_params();
 	hsv.tcp = NULL;
 	hsv.Create_HSV_trackbars();
+	hsv.Send_first_HSV_params();
 
 	cv::Mat img_input = cv::imread(argv[1]);
+	cv::imshow("Raw image", img_input);
 	bool detected_obj,	detected_pipe;
 	float horizontal_obj,	angle_pipe;
 	float vertical_obj,	distance_pipe;
 	float size_obj;
-	
-	Cameras::Find_blobs(&img_input, &blobs, &detected_obj, &horizontal_obj, &vertical_obj, &size_obj);
+
+	for(char key = 'a'; key != 'q'; key = cv::waitKey(40)){
+		blobs.Definir_limites_separation(&hsv);
+		Cameras::Find_blobs(&img_input, &blobs, &detected_obj, &horizontal_obj, &vertical_obj, &size_obj);
+		cv::imshow("Blob", blobs.Get_img_blobs());
+	}
+
 	cv::imwrite("img_blobs.png", blobs.Get_img_blobs());
 	reco.Set_img(blobs.Get_img_blobs());
 	cv::Mat img_pipeline = reco.Trouver_ligne_principale(true, &detected_pipe, &angle_pipe, &distance_pipe);
