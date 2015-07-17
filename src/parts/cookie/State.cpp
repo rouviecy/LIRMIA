b3bfa66e48f2b3cd4517ac57b1fc8ceaead7ac6e
rouviecy <pi@cyril-rouviere.fr>
2015-07-17 14:29:49 -0500

    Removed gyro for Cookie robot

--- src/parts/cookie/State.cpp
+++ src/parts/cookie/State.cpp
@@ -20,14 +20,11 @@ void State::On_start(){}
 
 void State::IO(){
 	Link_input("compass_thxyz",	COMFLOAT, 3, compass_thxyz);
-	Link_input("imu_vthz",		COMFLOAT, 1, &imu_vthz);
 	Link_input("depth",		COMFLOAT, 1, &depth);
 	Link_input("t",			COMFLOAT, 1, &t);
 
 	Link_output("xyz",		COMFLOAT, 3, xyz);
 	Link_output("thxyz",		COMFLOAT, 3, thxyz);
-	Link_output("vthxyz",		COMFLOAT, 3, vthxyz);
-
 }
 
 void State::Job(){
@@ -35,7 +32,6 @@ void State::Job(){
 	thxyz[0] = compass_thxyz[0];
 	thxyz[1] = compass_thxyz[1];
 	thxyz[2] = compass_thxyz[2];
-	vthxyz[2] = imu_vthz;
 	xyz[2] = depth;
 	Critical_send();
 }
